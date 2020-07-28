867e65b73a87e2b93b31ea69b40b5c0be10ef0be
Joshua Lobo <47839761+JoshuaLobo@users.noreply.github.com>
2020-07-28 08:45:51 +0530

    Add files via upload

---
+++ Module4/script.js
@@ -0,0 +1,11 @@
+(function() {
+    var names = ["Preeti", "Jai Prakash", "Jyoti", "Jack", "Pramod", "Faizal", "Akanksha", "Crystell", "Joshua"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
